2e6da018f9652e5bc7335bde9d4a94dce175ee04
impart-security <support@impart.security>
2023-10-20 08:23:47 +0000

    Update aws log forwarder serverless

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# aws-log-forwarder-serverless
-AWS Lambda function for the log forwarding. Serverless version
+
+# AWS log forwarder serverless
+
+Lambda function to stream ec2 loadbalancer access logs and cloudwatch logs without hosting local impart inspector service.
+
+## Logstream ingestion configuration
+
+1. Create logstream access token. Open https://console.impartsecurity.net/orgs/_/settings/tokens => New API acces token => select scopes: `read:inspector_settings`, `write:inspector_metrics`, `write:org_logstream`
+2. Add access token to the aws parameter store or secret manager
+3. Create a [Log Binding](https://console.impartsecurity.net/orgs/_/log-bindings).
+   Specify grok pattern for the expected log format.
+
+   The following fields are supported and required to be resolved:
+
+   - timestamp - request timestamp, `HTTPDATE` and `TIMESTAMP_ISO8601` time formats are supported automatically. For custom time format provide layout in the grok: `%{GREEDYDATA:timestamp:ts-"2006-01-02 15:04:05.000"}`
+   - request - request url. Can include query string parameters if available
+   - response_code - response status code
+   - http_method - request http method
+
+4. Configure aws forwarder lambda function with the environment variables:
+```
+ACCESS_TOKEN_PARAMETER_NAME: "<parameter store name from the step(2)>"
+LOGSTREAM_ID: "<from the logbinding setup step(3)>"
+```
+or
+```
+ACCESS_TOKEN_SECRET_NAME: "<secrets manager secret name from the step(2)>" 
+LOGSTREAM_ID: "<from the logbinding setup step(3)>"
+```
+
+### Grok Examples
+
+For elb access logs:
+
+```
+%{TIMESTAMP_ISO8601:timestamp} %{NOTSPACE:loadbalancer} %{IP:client_ip}:%{NUMBER:client_port} (?:%{IP:backend_ip}:%{NUMBER:backend_port}|-) %{NUMBER:request_processing_time} %{NUMBER:backend_processing_time} %{NUMBER:response_processing_time} (?:%{NUMBER:response_code}|-) (?:%{NUMBER:backend_status_code}|-) %{NUMBER:received_bytes} %{NUMBER:sent_bytes} "(?:%{WORD:http_method}|-) (?:%{GREEDYDATA:request}|-) (?:HTTP/%{NUMBER:http_version}|-( )?)" "%{DATA:user_agent}"( %{NOTSPACE:ssl_cipher} %{NOTSPACE:ssl_protocol})?
+```
+
+For api gateway cloudwatch access logs if the log format set to:
+
+```
+$context.requestTime "$context.httpMethod $context.path $context.protocol" $context.status $context.identity.sourceIp $context.requestId
+```
+
+```
+%{HTTPDATE:timestamp} "(?:%{WORD:http_method}|-) (?:%{GREEDYDATA:request}|-) (?:HTTP/%{NUMBER:http_version}|-( )?)" (?:%{NUMBER:response_code}|-)
+```
+
+
+
+
---
+++ index.js
@@ -0,0 +1,159 @@
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import axios from "axios";
+import zlib from "zlib";
+import util from "util";
+import readline from "readline";
+import {Stream} from 'stream';
+
+const gunzip = util.promisify(zlib.gunzip);
+const apiBaseUrl = process.env.API_BASE_URL ?? "https://api.impartsecurity.net/v0";
+const accessTokenParameter = process.env.ACCESS_TOKEN_PARAMETER_NAME;
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET_NAME;
+const logstreamId = process.env.LOGSTREAM_ID;
+
+const EventTypes = {
+  CloudWatch: 0,
+  S3: 1,
+}
+
+if (!accessTokenParameter && !accessTokenSecret) {
+  const err = "missing ACCESS_TOKEN_PARAMETER_NAME or ACCESS_TOKEN_SECRET_NAME env variable";
+  console.log(err);
+  process.exit(-1);
+}
+
+const fetchParameter = async () => {
+  const ssmClient = new SSMClient({ })
+  const command = new GetParameterCommand({ Name: accessTokenParameter, WithDecryption: true });
+  const response = await ssmClient.send(command);
+  return response.Parameter.Value;
+}
+
+const fetchSecret = async () => {
+  const secretsManagerClient = new SecretsManagerClient({ })
+  const command = new GetSecretValueCommand({
+    SecretId: accessTokenSecret
+  });
+  const response = await secretsManagerClient.send(command);
+  return response.SecretString;
+}
+
+const accessToken = accessTokenParameter? await fetchParameter() : await fetchSecret();
+
+export const handler = async (event, context, callback) => {
+  if (!logstreamId) {
+    const err = "missing LOGSTREAM_ID env variable";
+    console.log(err);
+    callback(err);
+    return;
+  }
+
+  const arr = accessToken.split(".");
+  if (arr.length < 2){
+    console.log("invalid access token value");
+    callback("invalid access token value");
+    return;
+  }
+
+  const decoded = JSON.parse(
+    Buffer.from(arr[1], "base64").toString()
+  );
+  const orgId = decoded.sub.substring("4");
+  const readableStream = new Stream.Readable( {
+    read( ) { }
+  })
+  let lineCount = 0;
+
+  let eventType = 0;
+  if (event.awslogs) {
+    eventType = EventTypes.CloudWatch;
+  } else if (event.Records[0].s3) {
+    eventType = EventTypes.S3;
+  }
+  else {
+     callback("unsupported event type");
+     return;
+  }
+
+  const url = `${apiBaseUrl}/orgs/${orgId}/logstream/${logstreamId}`;
+  //initiate send stream request
+  const promise = axios.post(url, readableStream, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/octet-stream',
+      'User-Agent': 'aws-labmda-forwarder'
+    },
+  }).then(() => {
+    console.log(`sent ${lineCount} lines for inspection`);
+    callback(null, `sent ${lineCount} lines for inspection`);
+  }).catch(function (error) {
+    console.log(error.response.status);
+    console.log(error.response.data);
+    callback(error.response.data)
+  });
+
+  switch (eventType) {
+    case EventTypes.CloudWatch: {
+      console.log("awslogs event");
+      const payload = new Buffer.from(event.awslogs.data, "base64"); // decode base64 to binary
+      const result = await gunzip(payload);
+      const parsedRequest = JSON.parse(result.toString("utf8"));
+
+      for (let i = 0; i < parsedRequest.logEvents.length; i++) {
+        if (
+          parsedRequest.logEvents[i].message.length &&
+          parsedRequest.logEvents[i].message[0] === "#"
+        ) {
+          continue;
+        }
+
+        const message = parsedRequest.logEvents[i].message.endsWith("\n")
+          ? parsedRequest.logEvents[i].message
+          : parsedRequest.logEvents[i].message + "\n";
+
+        readableStream.push(message);
+        ++lineCount;
+      }
+
+      readableStream.push(null);//end of stream
+      break;
+    }
+    case EventTypes.S3: {
+      const bucket = event.Records[0].s3.bucket.name;
+      console.log("S3 bucket: ", bucket);
+      const key = decodeURIComponent(
+        event.Records[0].s3.object.key.replace(/\+/g, " "),
+      );
+
+      // Retrieve S3 Object
+      const s3Client = new S3Client();
+      const getObjectCommand = new GetObjectCommand({
+        Bucket: bucket,
+        Key: key,
+      });
+
+      const response = await s3Client.send(getObjectCommand);
+      const lineReader = readline.createInterface({
+        input: response.Body.pipe(zlib.createGunzip()),
+      });
+
+      lineReader.on("line", (line) => {
+        if (line[0] !== "#") {
+          readableStream.push(line + "\n");
+          ++lineCount;
+        }
+      });
+
+      lineReader.on("close", () => {
+        readableStream.push(null);//end of stream
+      });
+      break;
+    }
+    default:
+      callback("unknown event type");
+  }
+
+  await promise;
+};
